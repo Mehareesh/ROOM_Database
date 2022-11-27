34904e7bfb5f32916315ab4d9f779fd983243006
Mehareesh Meruga <mehareeshm@gmail.com>
2022-11-27 16:36:32 +0530

    Added Repository and ViewModel classes to achieve clean architecture

---
+++ app/src/main/java/com/mmeruga/roomdatabase/repository/UserRepository.kt
@@ -0,0 +1,22 @@
+package com.mmeruga.roomdatabase.repository
+
+import androidx.lifecycle.LiveData
+import com.mmeruga.roomdatabase.database.User
+import com.mmeruga.roomdatabase.database.UserDao
+
+class UserRepository(private val userDao: UserDao) {
+
+    val readAllUserData: LiveData<List<User>> = userDao.getAllUsers()
+
+    suspend fun addUser(user: User) {
+        userDao.addUser(user)
+    }
+
+//    fun updateUser(user: User) {
+//        userDao.updateUser(user)
+//    }
+//
+//    fun deleteUser(id: Int) {
+//        userDao.deleteUser(id)
+//    }
+}
---
+++ app/src/main/java/com/mmeruga/roomdatabase/repository/UserViewModel.kt
@@ -0,0 +1,29 @@
+package com.mmeruga.roomdatabase.repository
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.mmeruga.roomdatabase.database.User
+import com.mmeruga.roomdatabase.database.UserDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class UserViewModel(application: Application): AndroidViewModel(application) {
+
+    private val readAllData: LiveData<List<User>>
+    private val repository: UserRepository
+
+
+    init {
+        val userDao = UserDatabase.getDatabase(application).getUserDao()
+        repository = UserRepository(userDao)
+        readAllData = repository.readAllUserData
+    }
+
+    fun addUser(user: User) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.addUser(user)
+        }
+    }
+}
